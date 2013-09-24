ea22f640140b5692fc4bc4af4e80b2993542fb95
sparkozos <gperatinos@gmail.com>
2013-09-24 13:05:46 +0300

    Initiliaze repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Malakas1::Application.config.secret_key_base = '54aee8f5c7bda3fc6e2fd85740be23d73884a157ac89bdaba4ea2e857d8c430bdf573d58428abc55c87d49623c0da9b7f28f1a6192b9f33b1d3e8062b754a95d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Malakas1::Application.config.session_store :cookie_store, key: '_malakas1_session'
